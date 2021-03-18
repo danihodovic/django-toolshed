64afd17cd7fd31aab4c96ba4b86f837a8fe7d97b
Dani Hodovic <dani.hodovic@gmail.com>
2021-03-18 21:52:19 +0200

    Cookiecutter

---
+++ README.md
@@ -0,0 +1,3 @@
+# django tools
+
+Your project description goes here
---
+++ django_toolshed/apps.py
@@ -0,0 +1,12 @@
+from django.apps import AppConfig
+
+
+class DjangoToolsConfig(AppConfig):
+    name = 'django_toolshed'
+
+    def ready(self):
+        try:
+            # pylint: disable=unused-import
+            import django_toolshed.signals
+        except ImportError:
+            pass
