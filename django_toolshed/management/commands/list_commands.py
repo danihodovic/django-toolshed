ba3af6d2815ecc61c8829c0ab8987c8dabcb6c04
Dani Hodovic <dani.hodovic@gmail.com>
2021-07-08 15:05:50 +0200

    Add command to list and pipe commands through fzf

---
+++ django_toolshed/management/commands/list_commands.py
@@ -0,0 +1,24 @@
+from django.core import management
+import djclick as click
+from iterfzf import iterfzf
+
+def format_cmd(app, command_name):
+    return f"{command_name:35s}{app}"
+
+@click.command()
+@click.option(
+    "--fzf/--no-fzf",
+    default=True,
+    show_default=True,
+)
+def command(fzf):
+    commands = management.get_commands()
+    commands_list = [format_cmd(app, command) for command, app in commands.items()]
+    if not fzf:
+        click.echo("\n".join(commands_list))
+        return
+    chosen = iterfzf(iter(commands_list))
+    if not chosen:
+        return
+    chosen = chosen.split()[0]
+    click.echo(f"./manage.py {chosen}", nl=False)
